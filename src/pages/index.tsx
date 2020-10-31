1c56ee933da1906e6f531ba2b3280ca36cd1b262
Adrien HARNAY <adrien@harnay.me>
2020-10-31 21:24:22 +0100

    home page OK

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,22 @@ import PostCard from 'src/components/post-card/PostCard';
 
 import { getSortedPostsData } from 'src/utils/posts';
 
+const availableForContractWorkPost: ReturnType<
+  typeof getSortedPostsData
+>[number] = {
+  id: 'skills',
+  cover: '/images/posts/laptop-code.jpeg',
+  date: '',
+  title: "I'm available for contract work!",
+  excerpt:
+    "Do you need to build a MVP for your start-up? Or a new feature for your product but your teams don't have the time? Do you need to implement a new solution and want someone with experience to do it and/or teach your team? Reach out to me!",
+};
+
 export const getStaticProps = async () => {
   const posts = getSortedPostsData();
   return {
     props: {
-      posts,
+      posts: [...posts, availableForContractWorkPost],
     },
   };
 };
