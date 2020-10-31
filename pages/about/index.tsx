d881e933e0d5c725afc0f6c70950d1105c0862e8
Adrien HARNAY <adrien@harnay.me>
2020-10-31 15:21:53 +0100

    [feat] about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,15 +1,38 @@
 import React, { FunctionComponent } from 'react';
 import Head from 'next/head';
-import MainLayout from '../../layouts/main-layout/MainLayout';
 
-const AboutPage: FunctionComponent = () => {
+import MainLayout from '../../layouts/main-layout/MainLayout';
+import { getAboutData } from '../../utils/about';
+
+import styles from '../[postId]/index.module.scss';
+
+export const getStaticProps = async () => {
+  const about = await getAboutData();
+  return {
+    props: {
+      about,
+    },
+  };
+};
+
+type AboutPageProps = {
+  about: UnwrapPromise<ReturnType<typeof getAboutData>>;
+};
+
+const AboutPage: FunctionComponent<AboutPageProps> = ({ about }) => {
   return (
     <>
       <Head>
         <title>About</title>
       </Head>
       <MainLayout>
-        <div>About</div>
+        <section className={`${styles.content} ${styles.noImg}`}>
+          <div
+            className={styles.markdownContainer}
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: about.html }}
+          />
+        </section>
       </MainLayout>
     </>
   );
