4a2379cd7c903ced0d81df81e920de90e6cb88b9
Adrien HARNAY <adrien@harnay.me>
2020-11-01 16:27:08 +0100

    wip long section

--- src/pages/skills/components/SkillsLongSection.tsx
+++ src/pages/skills/components/SkillsLongSection.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import styles from './SkillsLongSection.module.scss';
+
 const SkillsLongSection = () => {
-  return <div />;
+  return (
+    <section>
+      <h2 className={styles.title}>In depth</h2>
+      <p>
+        <em>
+          Each skill listed here has been used in a professional setting, for
+          several years for the vast majority. I like to try new things on side
+          projects so that I can later on introduce them in my work, but I stick
+          to my area of expertise when working on professional projects.
+        </em>
+      </p>
+    </section>
+  );
 };
 
 export default SkillsLongSection;
